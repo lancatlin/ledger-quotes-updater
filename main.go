dc006af9b0e4c39f6d721ffaff49ec9b67b63c40
wancat <wancat@wancat.cc>
2022-06-01 17:45:43 +0800

    Remove recursive price transform

--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"bufio"
 	"encoding/json"
-	"fmt"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"strings"
 	"time"
 )
@@ -20,8 +19,8 @@ type Quote struct {
 			Response []struct {
 				Meta struct {
 					RegularMarketPrice float64 `json:regularMarketPrice`
-					Currency string `json:currency`
-					RegularMarketTime uint64 `json:regularMarketTime`
+					Currency           string  `json:currency`
+					RegularMarketTime  uint64  `json:regularMarketTime`
 				} `json:meta`
 			} `json:response`
 		} `json:result`
@@ -32,19 +31,16 @@ const API = "https://query1.finance.yahoo.com/v7/finance/spark?symbols=%s&range=
 
 type Price struct {
 	Commodity string
-	Currency string
-	Price float64
+	Currency  string
+	Price     float64
 }
 
 func main() {
-	ledgerBinary := flag.String("b", "ledger", "Ledger Binary")
-	ledgerFile := flag.String("f", "ledger.ledger", "Ledger File")
 	priceDbFile := flag.String("p", "prices.db", "Price Database File")
 	mappingFile := flag.String("m", "mapping", "Commodities Name Mapping File")
 	flag.Parse()
 
 	mappings := GetMapping(*mappingFile)
-	commodities := GetCommodities(*ledgerFile, *ledgerBinary)
 
 	pricedb, err := os.OpenFile(*priceDbFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -56,24 +52,11 @@ func main() {
 	log.Println("Currency", currency)
 	prices := make(map[string]Price)
 
-	start := time.Now()
-	for i, c := range commodities {
-		if i+1%5 == 0 {
-			elapsed := time.Now().Sub(start)
-			if elapsed < time.Minute {
-				log.Println("Sleeping because of API limit")
-				time.Sleep(time.Minute - elapsed)
-			}
-			start = time.Now()
-		}
+	for c, ticker := range mappings {
 		if c == "$" {
 			continue
 		}
 
-		ticker := c
-		if value, ok := mappings[c]; ok {
-			ticker = value
-		}
 		price, err := GetPriceString(c, ticker)
 		if err != nil {
 			log.Println("Skipped " + c)
@@ -83,23 +66,13 @@ func main() {
 	}
 
 	for c, p := range prices {
-		result := fmt.Sprintf("P %s %s $%f\n", GetTimeString(), c, p.GetPrice(currency, prices))
+		if p.Currency == currency {
+			p.Currency = "$"
+		}
+		result := fmt.Sprintf("P %s %s %f %s\n", GetTimeString(), c, p.Price, p.Currency)
 		pricedb.WriteString(result)
 	}
 	log.Println("Stock price update complete")
-}
-
-func (p Price) GetPrice(currency string, prices map[string]Price) float64 {
-	if p.Commodity == currency {
-		return 1
-	}
-	if p.Currency == currency {
-		return p.Price
-	}
-	if altCurrency, ok := prices[p.Currency]; ok {
-		return p.Price * altCurrency.GetPrice(currency, prices)
-	}
-	return 0
 }
 
 func GetPriceString(commodity, ticker string) (price Price, err error) {
@@ -122,31 +95,14 @@ func GetPriceString(commodity, ticker string) (price Price, err error) {
 
 	price = Price{
 		Commodity: commodity,
-		Price: f.Spark.Result[0].Response[0].Meta.RegularMarketPrice,
-		Currency: f.Spark.Result[0].Response[0].Meta.Currency,
+		Price:     f.Spark.Result[0].Response[0].Meta.RegularMarketPrice,
+		Currency:  f.Spark.Result[0].Response[0].Meta.Currency,
 	}
 	return
 }
 
 func GetTimeString() string {
 	return time.Now().Format("2006-01-02 15:04:05")
-}
-
-func GetCommodities(ledger string, binary string) []string {
-	cmd := exec.Command(binary, "-f", ledger, "commodities")
-	out, err := cmd.Output()
-	if err != nil {
-		log.Fatalf("Ledger file commodity report failed with %s\n", err)
-	}
-	a := strings.Split(string(out), "\n")
-	sliceOut := a[:len(a)-1]
-
-	commodities := make([]string, 0)
-	for _, e := range sliceOut {
-		e = strings.Trim(e, `"`)
-		commodities = append(commodities, e)
-	}
-	return commodities
 }
 
 func GetMapping(mappingFile string) map[string]string {
